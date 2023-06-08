30a6f8fe16f6e442bc606d5f419a8942878207d4
FredyKwa83 <profekwa@gmail.com>
2023-06-08 18:30:10 -0500

    Agregue formulario

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(3001, () => {
+app.listen(3005, () => {
     console.log("servidor corriendo")
 })
 
 app.get("/mio" , (req, res) => {
     res.sendFile(path.join(__dirname, "./view/index.html"))
 });
+
+app.get("/ingresar", (req, res) => {
+    res.sendFile(path.join(__dirname, "./view/ingresar.html"))
+});
+
