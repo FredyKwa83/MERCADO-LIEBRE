ed5da4533fa3a090e10d5254cd41cc3d3b7efdf8
FredyKwa83 <profekwa@gmail.com>
2023-06-10 06:24:12 -0500

    modifique el tamaño de resolucion del @media

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "./public")));
 
-app.listen(3005, () => {
+app.listen(3006, () => {
     console.log("servidor corriendo")
 })
 
